c9f68e79f7786315fbbbf4f56b7251ab403a56d1
Gaetan Lembert <gaetan.lembert@etu.uca.fr>
2023-05-15 16:14:52 +0200

    le fichier affichage_joy.py retourne les ordres donner sur le joystick

--- mvt_joystick/script/affichage_joy.py
+++ mvt_joystick/script/affichage_joy.py
@@ -4,10 +4,30 @@ import rospy
 from sensor_msgs.msg import Joy
 
 def callback(msg):
-    print('ok')
+    x=msg.axes[0]
+    y=msg.axes[1]
+    print('le robot doit:')
+    if y>0.1:
+        print('avancer')
+    elif y<-0.1:
+        print('reculer')
+    else:
+        print('etre arreter')
+    
+    print('et doit aller:')
+    if x>0.1:
+        print('a gauche')
+    elif x<-0.1:
+        print('a droite')
+    else:
+        print('tout droit')
+    
+    #print('ok')
     
 rospy.init_node('affichage_joy')
 
 rospy.Subscriber("joy", Joy, callback)
       
 rospy.spin()
+
+
